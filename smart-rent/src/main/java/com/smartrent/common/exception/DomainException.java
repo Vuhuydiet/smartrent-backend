e7e04de039953ecac5b44f43d5af11ffa8e5349c
Huynh Tran Ty <22120418@student.hcmus.edu.vn>
2025-08-17 15:50:12 +0700

    solve conflict

--- smart-rent/src/main/java/com/smartrent/common/exception/DomainException.java
+++ smart-rent/src/main/java/com/smartrent/common/exception/DomainException.java
@@ -1,9 +1,6 @@
 package com.smartrent.common.exception;
 
 public class DomainException extends RuntimeException {
-<<<<<<< HEAD
-    
-=======
 
     private final DomainCode domainCode;
 
@@ -15,5 +12,4 @@ public class DomainException extends RuntimeException {
     public DomainCode getDomainCode() {
         return this.domainCode;
     }
->>>>>>> cbf0b5c8b7e5f97ed992c6bb45574048d15cfa7e
 }
--- smart-rent/src/test/java/com/smartrent/SmartRentApplicationTests.java
+++ smart-rent/src/test/java/com/smartrent/SmartRentApplicationTests.java
@@ -8,11 +8,7 @@ class SmartRentApplicationTests {
 
     @Test
     void contextLoads() {
-<<<<<<< HEAD
-        // Test chỉ đảm bảo context load thành công
-=======
         // Test to make sure the application starts successfully
->>>>>>> cbf0b5c8b7e5f97ed992c6bb45574048d15cfa7e
 		assert true;
     }
 }
