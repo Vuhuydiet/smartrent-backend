9695af72c638006bcac98e70964f97278fe5fe79
vuhuydiet <Truongvu04072004@gmail.com>
2025-10-05 18:15:42 +0700

    SR-0 refactor sms service

--- smart-rent/src/main/java/com/smartrent/service/sms/SmsService.java
+++ smart-rent/src/main/java/com/smartrent/service/sms/SmsService.java
@@ -1,8 +1,8 @@
 package com.smartrent.service.sms;
 
 import com.smartrent.dto.request.MockSmsRequest;
-import com.smartrent.dto.response.MockSmsResponse;
+import com.smartrent.dto.response.SmsResponse;
 
 public interface SmsService {
-    MockSmsResponse sendMessage(MockSmsRequest smsRequest);
+    SmsResponse sendMessage(MockSmsRequest smsRequest);
 }
